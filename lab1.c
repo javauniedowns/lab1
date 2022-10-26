a06ee8d4e8307482679394142881c4491a3724de
javauniedowns <78046266+javauniedowns@users.noreply.github.com>
2022-10-26 01:42:48 -0400

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -2,4 +2,36 @@
  * Implement your solution in thi file
  *
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.c"
 
+char* readString(char* fileName)
+{
+    char *pointer = malloc(100);
+
+    FILE *pointer2 = fopen(fileName,"r");
+    fgets(pointer, 100, pointer2);
+
+    return pointer;
+}
+
+char* mysteryExplode(const char* string)
+{
+
+    int length = strlen(string);
+    char *pointer3 = malloc(((length*length + length)/2)+1);
+
+    int i;
+    int j;
+    for(i = 1; i<strlen(string); i++)
+    {
+        for(j = 0; j<i; j++)
+        {
+            strncat(pointer3, &string[j], 1);
+        }
+    }
+
+    return pointer3;
+}
